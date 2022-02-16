0d4c73bdf1973a0b9312f6c967cba8c3fc851a86
soorajks21 <soorajks@live.in>
2022-02-16 01:37:29 -0500

    Feat : Error Handler
    Add common Error Handling middlerware to catch exceptions

---
+++ middleware/errorMiddleware.js
@@ -0,0 +1,18 @@
+const notFound = (req, res, next) => {
+  console.log('error 2 not found');
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  console.log('error');
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+module.exports = { notFound, errorHandler };
